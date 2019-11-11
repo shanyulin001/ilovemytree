75c3d77f6e61e853e00bf538d34bac47e0f49992
Your Name <you@example.com>
2019-11-11 23:09:05 +0800

    第一天，修改了一些页面2

--- src/views/user/tixian.vue
+++ src/views/user/tixian.vue
@@ -5,10 +5,16 @@
     </div>
     <div style="text-align:left">
         <van-cell title="支付宝" is-link value="15522708121" />
-    </div>
-    <div style="text-align:left">
         <van-cell title="提现金额" label-class="large" border label="￥300" />
+        <van-cell title="可提现金额：500元" value="全部提现" label="提现手续费：0.5元" />
     </div>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+    <van-button plain hairline type="info"  size="large" round style="width:90%;margin-top:15px;">确认提现</van-button>
+     <div><p class="phone">客服电话：110</p> </div>
     </div>
 </template>
 <style>
